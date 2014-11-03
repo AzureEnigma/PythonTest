3ab82c12f495c554c95581e6d6ac17e88c1ae539
AzureEnigma <dmangin@calpoly.edu>
2014-11-02 22:24:08 -0800

    egwhg

--- LegInfo-Accessor.py
+++ LegInfo-Accessor.py
@@ -8,8 +8,8 @@ query_insert_lobbying_firm = "INSERT INTO LobbyingFirm (filer_naml, filer_id, rp
 
 def insert_lobbying_firm(cursor, filer_naml, filer_id, rpt_date, ls_beg_yr, ls_end_yr):
 	print "INSERTING naml = {0}, id = {1}, date = {2}, beg = {3}, end = {4}\n".format(filer_naml, filer_id, rpt_date, ls_beg_yr, ls_end_yr)
-	cursor.execute("SELECT * FROM LobbyingFirm where filer_id = ?", (filer_id, ))
-	cursor.execute(query_insert_lobbying_firm, (filer_naml, filer_id, rpt_date, int(ls_beg_yr), int(ls_end_yr)))
+	if(db.query(filer_id).filter().count() > 0):
+		cursor.execute(query_insert_lobbying_firm, (filer_naml, filer_id, rpt_date, int(ls_beg_yr), int(ls_end_yr)))
 
 db = mysql.connector.connect(user = 'root', db = 'tester', password = '')
 dd = db.cursor(buffered = True)
