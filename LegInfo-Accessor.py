a2701664cf001d1026b6a932b7b7e989637a33c5
AzureEnigma <dmangin@calpoly.edu>
2014-11-02 23:10:16 -0800

    Try this on for size

--- LegInfo-Accessor.py
+++ LegInfo-Accessor.py
@@ -7,8 +7,9 @@ query_insert_lobbying_firm = "INSERT INTO LobbyingFirm (filer_naml, filer_id, rp
 
 def insert_lobbying_firm(cursor, filer_naml, filer_id, rpt_date, ls_beg_yr, ls_end_yr):
 	select_stmt = "SELECT filer_id FROM LobbyingFirm WHERE filer_id = %(filer_id)s"
-	cursor.execute(select_stmt, (filer_id))
-	cursor.execute(query_insert_lobbying_firm, (filer_naml, filer_id, rpt_date, int(ls_beg_yr), int(ls_end_yr)))
+	cursor.execute(select_stmt, {'filer_id':filer_id})
+	if(cursor.rowcount == 0):
+		cursor.execute(query_insert_lobbying_firm, (filer_naml, filer_id, rpt_date, ls_beg_yr, ls_end_yr))
 
 db = mysql.connector.connect(user = 'root', db = 'tester', password = '')
 dd = db.cursor(buffered = True)
@@ -33,6 +34,10 @@ try:
 				rpt_date = row[12]
 				ls_beg_yr = row[13]
 				ls_end_yr = row[14]
+				if ls_beg_yr:
+					ls_beg_yr = int(ls_beg_yr)
+				if ls_end_yr:
+					ls_end_yr = int(ls_end_yr)
 				print "naml = {0}, id = {1}, date = {2}, beg = {3}, end = {4}\n".format(filer_naml, filer_id, rpt_date, ls_beg_yr, ls_end_yr)
 				insert_lobbying_firm(dd, filer_naml, filer_id, rpt_date, ls_beg_yr, ls_end_yr)
 			elif form == "F604" and entity_cd == "LBY":
