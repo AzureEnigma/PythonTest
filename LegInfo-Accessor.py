ab2dfc7fdd1eb2896fa3de9b248db59f86fcf0ea
AzureEnigma <dmangin@calpoly.edu>
2014-11-02 22:27:46 -0800

    lame way

--- LegInfo-Accessor.py
+++ LegInfo-Accessor.py
@@ -8,9 +8,11 @@ query_insert_lobbying_firm = "INSERT INTO LobbyingFirm (filer_naml, filer_id, rp
 
 def insert_lobbying_firm(cursor, filer_naml, filer_id, rpt_date, ls_beg_yr, ls_end_yr):
 	print "INSERTING naml = {0}, id = {1}, date = {2}, beg = {3}, end = {4}\n".format(filer_naml, filer_id, rpt_date, ls_beg_yr, ls_end_yr)
-	if(db.query(filer_id).filter().count() > 0):
+	if filer_id not in ids:
+		ids.append(filer_id)
 		cursor.execute(query_insert_lobbying_firm, (filer_naml, filer_id, rpt_date, int(ls_beg_yr), int(ls_end_yr)))
 
+ids = {}
 db = mysql.connector.connect(user = 'root', db = 'tester', password = '')
 dd = db.cursor(buffered = True)
 
