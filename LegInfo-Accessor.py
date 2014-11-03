cbaaae2caedca66877ef39aadaffa83a9b6854de
AzureEnigma <dmangin@calpoly.edu>
2014-11-02 23:18:02 -0800

    Dates

--- LegInfo-Accessor.py
+++ LegInfo-Accessor.py
@@ -34,10 +34,6 @@ try:
 				rpt_date = row[12]
 				ls_beg_yr = row[13]
 				ls_end_yr = row[14]
-				if ls_beg_yr:
-					ls_beg_yr = int(ls_beg_yr)
-				if ls_end_yr:
-					ls_end_yr = int(ls_end_yr)
 				print "naml = {0}, id = {1}, date = {2}, beg = {3}, end = {4}\n".format(filer_naml, filer_id, rpt_date, ls_beg_yr, ls_end_yr)
 				insert_lobbying_firm(dd, filer_naml, filer_id, rpt_date, ls_beg_yr, ls_end_yr)
 			elif form == "F604" and entity_cd == "LBY":
