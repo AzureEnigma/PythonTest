4b8b4b36af0b4d1790d9fa8f3bebad910ccdfad9
AzureEnigma <dmangin@calpoly.edu>
2014-11-02 22:20:21 -0800

    das

--- LegInfo-Accessor.py
+++ LegInfo-Accessor.py
@@ -8,7 +8,7 @@ query_insert_lobbying_firm = "INSERT INTO LobbyingFirm (filer_naml, filer_id, rp
 
 def insert_lobbying_firm(cursor, filer_naml, filer_id, rpt_date, ls_beg_yr, ls_end_yr):
 	print "INSERTING naml = {0}, id = {1}, date = {2}, beg = {3}, end = {4}\n".format(filer_naml, filer_id, rpt_date, ls_beg_yr, ls_end_yr)
-	cursor.execute("SELECT * FROM Lobbying_Firm where filer_id = ?", (filer_id, ))
+	cursor.execute("SELECT * FROM LobbyingFirm where filer_id = ?", (filer_id, ))
 	if(cursor.rowcount < 0):
 		cursor.execute(query_insert_lobbying_firm, (filer_naml, filer_id, rpt_date, int(ls_beg_yr), int(ls_end_yr)))
 
