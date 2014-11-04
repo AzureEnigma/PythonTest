934795585467179e1a2f665717fd070b0cdbb6f9
AzureEnigma <dmangin@calpoly.edu>
2014-11-04 15:51:14 -0800

    Check if date exists

--- LegInfo-Accessor.py
+++ LegInfo-Accessor.py
@@ -38,7 +38,8 @@ try:
 				filer_id = row[5]
 				rpt_date = row[12]
 				rpt_date = rpt_date[:-3]
-				rpt_date = format_date(rpt_date)
+				if rpt_date:
+					rpt_date = format_date(rpt_date)
 				ls_beg_yr = row[13]
 				ls_end_yr = row[14]
 				print "naml = {0}, id = {1}, date = {2}, beg = {3}, end = {4}\n".format(filer_naml, filer_id, rpt_date, ls_beg_yr, ls_end_yr)
