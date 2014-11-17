9b26edbfdfdf888e8e33b81bbb332639a7d69d68
AzureEnigma <dmangin@calpoly.edu>
2014-11-17 13:32:46 -0800

    Want to test the sql statement

--- committee_members.py
+++ committee_members.py
@@ -1,9 +1,30 @@
 import json
 import re
 import sys
+import mysql.connector
+from pprint import pprint
 from urllib import urlopen
 
-url = urlopen('openstates.org/api/v1/committees/')
+def find_committee():
+	print 'committee'
 
-print url
+#gets all of the committees in CA
+url = urlopen('http://openstates.org/api/v1/committees/?apikey=d1a1fe2c7d53443284d0ea62d8ce7dce&state=ca').read()
+result = json.loads(url)
+
+db = mysql.connector.connect(user = 'root', db = 'DDDB', password = '')
+dd = db.cursor(buffered = True)
+
+try:
+	select_stmt = ("SELECT * FROM Committee")
+	dd.execute(select_stmt)
+	print dd.fetchone()
+	print dd.fetchone()
+
+except:
+	db.rollback()
+	print 'error!', sys.exc_info()[0], sys.exc_info()[1]
+	exit()
+	
+db.close()
 
